5f901067687b76a946146080317fef8d4fda6f13
L3MNcakes <L3MNcakes@gmail.com>
2015-09-09 01:24:50 -0600

    Initial environment implementation

--- src/Environment.js
+++ src/Environment.js
@@ -8,17 +8,47 @@
 (function() {
     "use strict";
 
-    var _ = require("lodash");
+    var _ = require("lodash"),
+        Chromosome = require("./Chromosome");
 
     /**
      * Creates a new Environment
      *
      * @constructor Environment
-     * @param {Object} options - Options used to initialize the environment
-     * @param {Array}  options.chromosomes - An array of Chromosome objects to run through the algorithm
+     * @param {Object}      options - Options used to initialize the environment
+     * @param {Prototype}   options.cType - Your extended {@link Chromosome} implementation. If not given, the default
+     *                                     {@link Chromosome} prototype will be used.
+     * @param {Object}      options.cOptions - Options that will be passed to {@link Chromosome} constructor.
+     * @param {Number}      options.popSize - Initial size of the population
      **/
     var Environment = (function(options) {
+        options = options || {};
+        options.cType = _.isFunction(options.cType) ? options.cType : Chromosome;
 
+        if (!_.isNumber(options.popSize)) {
+            throw new TypeError("Environment.constructor : options.popSize must be a Number");
+        }
+
+        this.options = options;
+        this.initPop();
+    });
+
+    /**
+     * Initializes a new chromosome population of size options.popSize
+     **/
+    Environment.prototype.initPop = (function() {
+        this.population = [];
+
+        for (var i=0; i < this.options.popSize; i++) {
+            var cType = this.options.cType,
+                cOpts = this.options.cOptions,
+                c = new cType(cOpts);
+
+
+            this.population.push(c);
+        }
+
+        return;
     });
 
     module.exports = Environment;
--- test/Environment.js
+++ test/Environment.js
@@ -11,15 +11,33 @@ describe("Environment", function() {
      **/
     beforeEach(function() {
         this.TestChromosome = Chromosome.extend({
-
+            randomize: function() { return "Random"; },
+            mutate: function() { return "Mutate"; },
+            fitness: function() { return 100; }
         });
 
         this.testOptions = {
-
+            cType: this.TestChromosome.prototype,
+            cOptions: {},
+            popSize: 10
         };
     });
 
     describe("construction", function() {
+        it("sets correct options to the object", function() {
+            var env = new Environment(this.testOptions);
 
+            assert.strictEqual(env.options, this.testOptions);
+        });
+
+        it("throws TypeError when options.popSize is not a number", function() {
+            var testOptions = this.testOptions,
+                fn = (function() {
+                    testOptions.popSize = "NotANumber";
+                    new Environment(testOptions);
+                });
+
+            assert.throws(fn, TypeError, /options.popSize must be a Number/);
+        });
     });
 });
